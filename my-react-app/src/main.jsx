0890715d01a3b7fe3d9f4e1a8a613ad3f17df377
Akshi Biswal <biswalakshi@gmail.com>
2025-11-16 16:28:57 -0600

    Wrap App component with BrowserRouter
    Added BrowserRouter wrapper to enable routing.

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom' // <-- This import is crucial
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter> {/* <-- This wrapper makes all your routes work */}
       <App />
     </BrowserRouter>
   </StrictMode>,
